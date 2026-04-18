3dabec87d266b4c2387c853242f31698ebaa616d
sgkang234 <sgkang234@gmail.com>
2026-04-18 15:38:55 +0900

    두_수의_차_구하기

---
+++ src/level_0/두_수의_차_구하기.java
@@ -0,0 +1,28 @@
+package level_0;
+
+// 2026.04.18
+// 두 정수를 뺀 정수를 반환하는 문제
+// https://school.programmers.co.kr/learn/courses/30/lessons/120803
+
+public class 두_수의_차_구하기 {
+    public static void main(String[] args) {
+        // 입력
+        int num1 = 2;
+        int num2 = 3;
+
+        // 로직
+        int answer = num1 - num2;
+
+        // 출력
+        System.out.print(answer);
+    }
+}
+
+/*
+class Solution {
+    public int solution(int num1, int num2) {
+        int answer = num1 - num2;
+        return answer;
+    }
+}
+*/

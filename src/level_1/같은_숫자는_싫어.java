27d2ce9dec7adae2a6639af9b8e82fa9e946ea31
sgkang234 <sgkang234@gmail.com>
2026-04-19 18:36:51 +0900

    같은_숫자는_싫어

---
+++ src/level_1/같은_숫자는_싫어.java
@@ -0,0 +1,46 @@
+package level_1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class 같은_숫자는_싫어 {
+    public static void main(String[] args) {
+        // 입력
+        int[] arr = {1,1,3,3,0,1,1};
+
+        // 로직
+        List<Integer> answer = new ArrayList<>();
+        answer.add(arr[0]);
+        for (int i = 1; i < arr.length; i++) {
+            if(arr[i-1] != arr[i]){
+                answer.add(arr[i]);
+            }
+        }
+
+        // 출력
+        System.out.println(answer);
+    }
+}
+
+/*
+public class Solution {
+    public int[] solution(int []arr) {
+        List<Integer> answer = new ArrayList<>();
+
+        answer.add(arr[0]);
+
+        for (int i = 1; i < arr.length; i++) {
+            if(arr[i-1] != arr[i]){
+                answer.add(arr[i]);
+            }
+        }
+
+        int[] result = new int[answer.size()];
+        for (int i = 0; i < answer.size(); i++) {
+            result[i] = answer.get(i);
+        }
+
+        return result;
+    }
+}
+*/

e96f01a8d79cb9e55219abef7d87e62eaec2a8dc
sgkang234 <sgkang234@gmail.com>
2026-04-20 16:32:01 +0900

    나이_출력

---
+++ src/level_0/나이_출력.java
@@ -0,0 +1,19 @@
+package level_0;
+
+// 2026.04.20
+// 2022년 기준 선생님의 나이 age가 주어질 때 출생년도를 출력
+// https://school.programmers.co.kr/learn/courses/30/lessons/120820
+
+public class 나이_출력 {
+    public static void main(String[] args) {
+        // 입력
+        int age = 40;
+
+        // 로직
+        int year = 2022;
+        int answer = year - age + 1;
+
+        // 출력
+        System.out.println(age);
+    }
+}

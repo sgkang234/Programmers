8b99433c2d2e1ca5f1c94fe677d0f90ae6ea7e04
sgkang234 <sgkang234@gmail.com>
2026-04-21 17:43:39 +0900

    직각삼각형_출력하기

---
+++ src/level_0/직각삼각형_출력하기.java
@@ -0,0 +1,55 @@
+package level_0;
+
+// 2026.04.21
+// 문자열을 뒤집어 출력하는 문제
+// https://school.programmers.co.kr/learn/courses/30/lessons/120823
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 직각삼각형_출력하기 {
+    public static void main(String[] args) throws IOException {
+        // 입력
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        // 로직
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j < i; j++) {
+                sb.append("*");
+            }
+            sb.append("\n");
+        }
+
+        // 출력
+        System.out.println(sb);
+    }
+}
+
+/*
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        // 입력
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        // 로직
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j < i; j++) {
+                sb.append("*");
+            }
+            sb.append("\n");
+        }
+
+        // 출력
+        System.out.println(sb);
+    }
+}
+ */

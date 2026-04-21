564e10f360024d6d5ed5feeef33759c105d0d1fe
sgkang234 <sgkang234@gmail.com>
2026-04-21 17:43:20 +0900

    뒤집힌_문자열

---
+++ src/level_0/뒤집힌_문자열.java
@@ -0,0 +1,32 @@
+package level_0;
+
+// 2026.04.21
+// 문자열을 뒤집어 출력하는 문제
+// https://school.programmers.co.kr/learn/courses/30/lessons/120822
+
+public class 뒤집힌_문자열 {
+    public static void main(String[] args) {
+        // 입력
+        String my_string = "jaron";
+
+        // 로직
+        String answer = "";
+        for (int i = my_string.length() - 1; i >= 0; i--) {
+            answer += my_string.charAt(i);
+        }
+
+        System.out.println(answer);
+    }
+}
+
+/*
+class Solution {
+    public String solution(String my_string) {
+        String answer = "";
+        for (int i = my_string.length() - 1; i >= 0; i--) {
+            answer += my_string.charAt(i);
+        }
+        return answer;
+    }
+}
+ */

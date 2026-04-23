b6b867f18e52c6fad90f5f55211809895e241405
sgKang <sgKang234@gmail.com>
2026-04-23 14:38:43 +0900

    양꼬치

---
+++ src/level_0/양꼬치.java
@@ -0,0 +1,37 @@
+package level_0;
+
+// 2026.04.23
+// 양꼬치 가게는 10인분을 먹으면 음료수 하나를 서비스로 줍다고 할 때, 양꼬치는 1인분에 12,000원, 음료수는 2,000원일 때 가격을 출력하는 문제
+// https://school.programmers.co.kr/learn/courses/30/lessons/120830
+
+public class 양꼬치 {
+    public static void main(String[] args) {
+        // 입력
+        int n = 10;
+        int k = 3;
+
+        // 로직
+        int service = n/10;
+        if(k > 0) {
+            k -= service;
+        }
+        int answer = n *12000 + k * 2000;
+
+        // 출력
+        System.out.println(answer);
+    }
+}
+
+/*
+class Solution {
+    public int solution(int n, int k) {
+        int service = n/10;
+        System.out.println(service);
+        if(k > 0) {
+            k -= service;
+        }
+        int answer = n *12000 + k * 2000;
+        return answer;
+    }
+}
+ */

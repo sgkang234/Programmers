a7cfa295787f76b394153b54ac0f5e5156234344
sgKang <sgKang234@gmail.com>
2026-04-28 17:46:08 +0900

    _014_문자열을_정수로_바꾸기

---
+++ src/level_1/_014_문자열을_정수로_바꾸기.java
@@ -0,0 +1,18 @@
+package level_1;
+
+// 2026.04.27
+// 문자열을 정수로 바꾸는 문제
+// https://school.programmers.co.kr/learn/courses/30/lessons/12925
+
+public class _014_문자열을_정수로_바꾸기 {
+    public static void main(String[] args) {
+        // 입력
+        String s = "-1234";
+
+        // 로직
+        int answer = Integer.parseInt(s);
+
+        // 출력
+        System.out.println(answer);
+    }
+}

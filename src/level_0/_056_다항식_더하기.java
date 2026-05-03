7b0223f43e6acfaf9d70c64a375acbc096f8dfa5
sgKang <sgKang234@gmail.com>
2026-05-03 22:59:50 +0900

    _056_다항식_더하기

--- src/level_0/_056_다항식_더하기.java
+++ src/level_0/_056_다항식_더하기.java
@@ -50,3 +50,46 @@ public class _056_다항식_더하기 {
         System.out.println(answer);
     }
 }
+
+/*
+class Solution {
+    public String solution(String polynomial) {
+        int xSum = 0;
+        int numSum = 0;
+
+        String[] arr = polynomial.split(" \\+ ");
+
+        for (String s : arr) {
+            if (s.contains("x")) {
+                s = s.replace("x", "");
+
+                if (s.equals("")) {
+                    xSum += 1;   // "x"만 있는 경우
+                } else {
+                    xSum += Integer.parseInt(s);
+                }
+            } else {
+                numSum += Integer.parseInt(s);
+            }
+        }
+
+        String answer = "";
+
+        if (xSum > 0) {
+            if (xSum == 1) {
+                answer += "x";
+            } else {
+                answer += xSum + "x";
+            }
+        }
+
+        if (numSum > 0) {
+            if (!answer.equals("")) {
+                answer += " + ";
+            }
+            answer += numSum;
+        }
+        return answer;
+    }
+}
+ */

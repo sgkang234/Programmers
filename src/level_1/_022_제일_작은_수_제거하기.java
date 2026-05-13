fd526e82da1470ca5e7013b03c15db6052c4cfa4
sgKang <sgKang234@gmail.com>
2026-05-13 13:17:11 +0900

    _022_제일_작은_수_제거하기

--- src/level_1/_022_제일_작은_수_제거하기.java
+++ src/level_1/_022_제일_작은_수_제거하기.java
@@ -1,5 +1,9 @@
 package level_1;
 
+// 2026.05.04
+// 정수를 저장한 배열, arr 에서 가장 작은 수를 제거한 배열을 출력하는 문제
+// https://school.programmers.co.kr/learn/courses/30/lessons/12935
+
 import java.util.Arrays;
 
 public class _022_제일_작은_수_제거하기 {
@@ -23,8 +27,37 @@ public class _022_제일_작은_수_제거하기 {
             }
         }
 
-
         // 출력
         System.out.println(Arrays.toString(answer));
     }
 }
+
+/*
+class Solution {
+    public int[] solution(int[] arr) {
+
+        if (arr.length == 1) {
+            return new int[]{-1};
+        }
+
+        int min = arr[0];
+
+        for (int num : arr) {
+            if (num < min) {
+                min = num;
+            }
+        }
+
+        int[] answer = new int[arr.length - 1];
+        int index = 0;
+
+        for (int num : arr) {
+            if (num != min) {
+                answer[index++] = num;
+            }
+        }
+
+        return answer;
+    }
+}
+ */

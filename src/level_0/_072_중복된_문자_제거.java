c8cb9ce6cd3f8732ed71d69e87bb9a9805e7479d
sgKang <sgKang234@gmail.com>
2026-05-13 13:18:00 +0900

    _072_중복된_문자_제거

--- src/level_0/_072_중복된_문자_제거.java
+++ src/level_0/_072_중복된_문자_제거.java
@@ -1,7 +1,7 @@
 package level_0;
 
 // 2026.05.13
-// 1부터 13까지의 수에서, 1은 1, 10, 11, 12, 13 이렇게 총 6번 등장한다. 정수 i, j, k가 매개변수로 주어질 때, i부터 j까지 k가 몇 번 등장하는지 출력하는 문제
+// 중복된 문자를 제거하는 문제
 // https://school.programmers.co.kr/learn/courses/30/lessons/120888
 
 import java.util.LinkedHashSet;
@@ -12,14 +12,12 @@ public class _072_중복된_문자_제거 {
         // 입력
         String my_string = "We are the world";
 
-
         // 로직
         String answer = "";
         Set<String> set = new LinkedHashSet<>(); // 순서를 유지하고, 중복을 막는다.
         for( int i = 0; i < my_string.length(); i++ ){
             set.add(String.valueOf(my_string.charAt(i)));
         }
-
 
         for (String s : set) {
             answer += s;
